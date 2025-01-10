ddb05adc15cd2da9afe2fe723439309bee885f2c
alfonso.lorenzana@gmail.com <alfonso.lorenzana@gmail.com>
2025-01-09 23:08:04 -0500

    copied local files over

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import _inquirer from 'inquirer';
+
+// List the questions that you are going to ask the user, which will direct them. To the correct place with INQUIRER.
+// function questionPrompts() {
+//     _inquirer.prompt([
+//         {
+//             type: 'list', 
+//             name: 'options',
+//             message: 'Pick what you would like to do',
+//             choices: [
+//                 {
+//                     questionName: 'View All Departments',
+//                     value: 'VIEW ALL DEPARTMENTS',
+//                 },
+//                 {
+//                     questionName: 'View All Roles',
+//                     value: 'VIEW ALL ROLES',
+//                 },
+//                 {
+//                     questionName: 'View All Employees',
+//                     value: 'VIEW ALL EMPLOYEES',
+//                 },
+//                 {
+//                     questionName: 'Add A Department',
+//                     value: 'ADD A DEPARTMENT',
+//                 },
+//                 {
+//                     questionName: 'Add A Role',
+//                     value: 'ADD A ROLES',
+//                 },
+//                 {
+//                     questionName: 'Add A Employees',
+//                     value: 'ADD A EMPLOYEES',
+//                 },
+//                 {
+//                     questionName: 'Update Employee Role',
+//                     value: 'UPDATE EMPLOYEE ROLE',
+//                 },
+//                 {
+//                     questionName: 'Quit',
+//                     value: 'QUIT',
+//                 },
+
+//             ],
+//         },
+//     ])
+// }
+/*
+
+inquirer
+.prompt([
+
+])
+.then((answers) =>
+})
+.catch(error) => {
+if (error.isTtyError) {
+} else {
+ }
+}
+
+*/
